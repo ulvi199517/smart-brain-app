2322026470da4a90aa598fab648660db6a7f1d08
ulvi_199517 <ulvi199517@gmail.com>
2020-11-25 00:52:30 -0500

    fixed minor errors

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,11 +5,15 @@ const ImageLinkForm = ({onInputChange,onButtonSubmit}) => {
     return(
         <div>
             <p className='f3'>
-                {'This Magic Brain will detect faces in your pictures. Give it a try'}
+                {'This Magic Brain will detect face in your picture. Give it a try!'}
             </p>
             <div className='center'>
                 <div className='form center pa4 br3 shadow-5'>
-                <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange}/>
+                <input 
+                    className='f4 pa2 w-70 center' 
+                    type='text' 
+                    onChange={onInputChange} 
+                    placeholder="Paste any image url address"/>
                 <button 
                     className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple' 
                     onClick={onButtonSubmit}> Detect </button>
